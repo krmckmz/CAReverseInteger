9ec060789183fa367c42aa0d6954dfa50b27f13c
krmckmz <keremcokmez@gmail.com>
2022-09-11 15:02:09 +0300

    added alternative solution with number steps

--- CAReverseInteger/Program.cs
+++ CAReverseInteger/Program.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args)
     {
         int number = 1534236469;
-        var result = Reverse(number);
+        var result = ReverseBySteps(number);
         System.Console.WriteLine(result);
 
     }
@@ -12,7 +12,7 @@
         var text = x.ToString();
         var reversedText = string.Empty;
 
-        if (text[0] == '-')
+        if (x < default(int))
         {
             reversedText += "-";
             text = text.Substring(1, text.Length - 1);
@@ -32,5 +32,32 @@
             return returnValue;
         else
             return default(int);
+    }
+
+    public static long ReverseBySteps(int x)
+    {
+        long reversedNumber = default(long);
+        int numberTen = 10;
+
+        int lastDigit = x % (numberTen);
+        if (lastDigit == default(int))
+            return default(int);
+
+        var textLength = x.ToString().Length;
+
+        while (textLength > default(int))
+        {
+            var residiual = x % numberTen;
+            if (residiual != default(int))
+            {
+                long power = (long)Math.Pow(numberTen, textLength - 1);
+                var multiply = residiual * power;
+                reversedNumber = reversedNumber + multiply;
+            }
+            x = x / numberTen;
+            textLength -= 1;
+        }
+
+        return reversedNumber;
     }
 }
